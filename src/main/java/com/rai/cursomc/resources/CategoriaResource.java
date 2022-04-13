bf9b3ecb55a5f70dec6485903ba8a2ee8bd4b391
Rai Oliveira <rai_oliveira_10@hotmail.com>
2022-04-13 14:46:25 -0300

    Testando primeira classe dominio - Categoria

--- src/main/java/com/rai/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rai/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.rai.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rai.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cta1 = new Categoria(1,"informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cta1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
