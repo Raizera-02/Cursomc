c8d69c27d6e80fa015f3ca6766b9a6e5461e9422
Rai Oliveira <rai_oliveira_10@hotmail.com>
2022-04-14 17:08:57 -0300

    Criação do repository e service para Categoria

---
+++ src/main/java/com/rai/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.rai.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rai.cursomc.domain.Categoria;
+import com.rai.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
