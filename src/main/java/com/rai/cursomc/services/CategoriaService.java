e94f827505da9feacd85142e6c499efbb99228a9
Rai Oliveira <rai_oliveira_10@hotmail.com>
2022-05-02 15:33:17 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/rai/cursomc/services/CategoriaService.java
+++ src/main/java/com/rai/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rai.cursomc.domain.Categoria;
+import com.rai.cursomc.dto.CategoriaDTO;
 import com.rai.cursomc.repositories.CategoriaRepository;
 import com.rai.cursomc.services.exceptions.DataIntegrityException;
 import com.rai.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
