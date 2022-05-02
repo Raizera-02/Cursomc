7c0f3b41667a66b4c1073241df817e33cb11902e
Rai Oliveira <rai_oliveira_10@hotmail.com>
2022-05-02 19:48:14 -0300

    PUT, DELETE  e GET para cliente

--- src/main/java/com/rai/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/rai/cursomc/dto/ClienteDTO.java
@@ -2,30 +2,37 @@ package com.rai.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
-import com.rai.cursomc.domain.Categoria;
+import com.rai.cursomc.domain.Cliente;
 
-public class CategoriaDTO implements Serializable{
+public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
 	
 	@NotEmpty(message="Preenchimento Obrigatório")
-	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
 	
-	public CategoriaDTO() {
-	}
-
-	public CategoriaDTO(Categoria obj) {
-		id = obj.getId();
-		nome = obj.getNome();
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Email(message="E-mail inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		
 	}
 	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -41,6 +48,13 @@ public class CategoriaDTO implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+
 }
--- src/main/java/com/rai/cursomc/resources/ClienteResource.java
+++ src/main/java/com/rai/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.rai.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rai.cursomc.domain.Cliente;
+import com.rai.cursomc.dto.ClienteDTO;
 import com.rai.cursomc.services.ClienteService;
 
 @RestController
@@ -21,5 +30,37 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id){
 		Cliente obj = service.find(id);		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll(){
+		List<Cliente> list = service.findAll(); 
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());//Converte uma lista para outra lista
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction){
+		Page<Cliente> list = service.findPage(page,linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));// Converte cada obj da lista para um dto
+		return ResponseEntity.ok().body(listDto);
 	}
 }
