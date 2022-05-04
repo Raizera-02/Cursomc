4e29e0f88c750d727ae1016a8959fe37115f45a2
Rai Oliveira <rai_oliveira_10@hotmail.com>
2022-05-04 16:22:14 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/rai/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/rai/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.rai.cursomc.domain.Cliente;
+import com.rai.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
