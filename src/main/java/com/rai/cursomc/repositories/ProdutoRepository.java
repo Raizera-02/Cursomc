2254a2b77f64b82846adfa54a68613653286ca9a
Rai Oliveira <rai_oliveira_10@hotmail.com>
2022-05-05 16:34:32 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/rai/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/rai/cursomc/repositories/ProdutoRepository.java
@@ -1,13 +1,22 @@
 package com.rai.cursomc.repositories;
 
-import org.springframework.stereotype.Repository;
 
-import com.rai.cursomc.domain.Produto;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rai.cursomc.domain.Categoria;
+import com.rai.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
-	
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
